f86d0ddc3d76dd07d6456c2a0b251bdff87085c1
JorgeC98 <A01410342@itesm.mx>
2017-11-28 23:38:57 -0600

    Update Edge.java

--- src/ed_equiporojo_pfinal/Edge.java
+++ src/ed_equiporojo_pfinal/Edge.java
@@ -13,24 +13,29 @@ public class Edge {
     private int dest;
     private double weight;
 
+    //Consturctor por defecto
     public Edge(int dest) {
         this.dest = dest;
         this.weight = 0.0;
     }
 
+    //Consturctor con un elemento
     public Edge(int dest, double weight) {
-        this.dest = dest;
+        this.dest = dest; 
         this.weight = weight;
     }
 
+    //Obtener destino
     public double getDest() {
         return dest;
     }
 
+    //Obtener peso
     public double getWeight() {
         return weight;
     }
 
+    //Imprimir edge
     @Override
     public String toString() {
         return "Edge{" + "dest=" + dest + ", weight=" + weight + '}';
