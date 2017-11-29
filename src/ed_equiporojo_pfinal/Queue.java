8610a271ae63e3e272aa156497559c486716bd0d
JorgeC98 <A01410342@itesm.mx>
2017-11-28 22:49:27 -0600

    Update Queue.java

--- src/ed_equiporojo_pfinal/Queue.java
+++ src/ed_equiporojo_pfinal/Queue.java
@@ -14,17 +14,20 @@ public class Queue<T> {
     private Node<T> rear;
     private int size;
     
+    //Constructor por defecto
     public Queue(){
         front = rear = null;
         size = 0;
     }
     
+    //Constructor con primer elemento
     public Queue(T d){
         Node<T> node = new Node(d);
         front = rear = node;
-        size = 0;
+        size = 1;
     }
     
+    //Inserta un nuevo nodo al final de la fila
     public void enqueue(T d){
         Node node = new Node(d);
         if(isEmpty()){
@@ -36,6 +39,8 @@ public class Queue<T> {
             size++;
         }
     }
+    
+    //Elimina el primer valor de la fila
     public T dequeue(){
         if (isEmpty()){
             return null;
@@ -47,6 +52,7 @@ public class Queue<T> {
         }
     }
    
+    //Devuelve el valor del frente de la fila
     public T getFront(){
         if (isEmpty()){
         return null;
@@ -55,19 +61,23 @@ public class Queue<T> {
         }
     }
     
+    //Devuelve el tamaño de la fila
     public int getSize(){
         return size;
     }
     
+    //Está vacía?
     public boolean isEmpty(){
         return size==0;
     }
     
+    //Vaciar fila
     public void clearQueue(){
         front=rear=null;
         size=0;
     }
     
+    //Muestra el contenido de la fila
     public void showQueue(){
         System.out.println("front->");
         Node<T> p = front;
